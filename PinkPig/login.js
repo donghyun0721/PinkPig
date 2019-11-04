c15cf83b6d117c3d67bd4100b3e45983d3d8bada
donghyun <donghyun@DESKTOP-QNAU44G>
2019-11-04 21:30:21 +0900

    First Commit

---
+++ PinkPig/login.js
@@ -0,0 +1,29 @@
+﻿var login = {
+
+    init: function() {
+        this.event();
+    },
+
+     event : function() {
+        var t = document.getElementById('submit');
+        t.onclick = function(event) {
+            //location.href = "/Page/Main.html";
+            checkUser();
+        }
+    },
+};
+
+function checkUser() {
+    var id = $('#loginID').val();
+    var pw = $('#password').val();
+
+    if (id == 'admin' && pw == 'admin')
+    {
+        location.href = "/Page/Main.html";    
+        console.log("관리자 접근");
+    }    
+};
+
+$(function () {
+    login.init();
+});
